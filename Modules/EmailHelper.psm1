2f52fc54e9cf6df96a95fd271dad06e50da1a41a
OliverPerring <32413830+OliverPerring@users.noreply.github.com>
2023-12-19 17:20:53 +0000

    Further functions and naming convention updates

--- Modules/EmailHelper.psm1
+++ Modules/EmailHelper.psm1
@@ -43,3 +43,43 @@ function SendResponse {
 
     $Null = New-HaloAction -Action $ActionUpdate
 }
+
+function FindSendResponse {
+    param (
+        $Username,
+        $clientid,
+        $EmailMessage,
+        $TicketId
+    )
+
+    $EmailAddress = GetUserEmail -Username $Username -ClientId $clientid
+
+    if ($null = $EmailAddress) {
+        return $false
+    }
+
+    #Send the request through Halo to send an email. 
+    $dateArrival = (get-date((get-date).AddMinutes(-1)))
+    $dateEnd = (get-date) 
+    
+    $ActionUpdate = @{
+        ticket_id               = $TicketId
+        outcome                 = "Email User"
+        outcome_id              = 16
+        emailfrom               = "IT Support"
+        replytoaddress          = "helpdesk@aegis-group.co.uk"
+        emailto                 = $EmailAddress
+        #emailsubject           = ""
+        note                    = $EmailMessage
+        actionarrivaldate       = $dateArrival
+        actioncompletiondate    = $dateEnd
+        action_isresponse       = $false
+        validate_response       = $false
+        sendemail               = $true
+    }
+
+    $Null = New-HaloAction -Action $ActionUpdate
+
+    return $true
+}
+
