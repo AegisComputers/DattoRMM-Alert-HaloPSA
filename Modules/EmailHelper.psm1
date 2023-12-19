dd2b9eadf0025598a6a99a6a72d0df6f98620346
OliverPerring <32413830+OliverPerring@users.noreply.github.com>
2023-12-19 17:07:16 +0000

    Send Response Functions
    Halo Datto send email layer

--- Modules/EmailHelper.psm1
+++ Modules/EmailHelper.psm1
@@ -1,1 +1,36 @@
+function GetUserEmail {
+    param (
+        $Username
+    )
+    #Get Email address based on provided username. 
+    
+}
 
+function SendResponse {
+    param (
+        $EmailAddress,
+        $EmailMessage,
+        $TicketId
+    )
+    #Send the request through Halo to send an email. 
+    $dateArrival = (get-date((get-date).AddMinutes(-1)))
+    $dateEnd = (get-date) 
+    
+    $ActionUpdate = @{
+        ticket_id               = $TicketId
+        outcome                 = "Email User"
+        outcome_id              = 16
+        emailfrom               = "IT Support"
+        replytoaddress          = "helpdesk@aegis-group.co.uk"
+        emailto                 = $EmailAddress
+        #emailsubject           = ""
+        note                    = $EmailMessage
+        actionarrivaldate       = $dateArrival
+        actioncompletiondate    = $dateEnd
+        action_isresponse       = $false
+        validate_response       = $false
+        sendemail               = $true
+    }
+
+    $Null = New-HaloAction -Action $ActionUpdate
+}
