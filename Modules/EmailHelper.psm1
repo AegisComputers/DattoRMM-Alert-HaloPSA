013c1b4368147e921bd8db93bdd313eb8fd616db
OliverPerring <32413830+OliverPerring@users.noreply.github.com>
2023-12-20 11:06:33 +0000

    Refactor functions

--- Modules/EmailHelper.psm1
+++ Modules/EmailHelper.psm1
@@ -1,85 +1,141 @@
-function GetUserEmail {
+<#
+.SYNOPSIS
+Retrieves the email address for a specified Halo user.
+
+.DESCRIPTION
+This function fetches the email address associated with a given Halo user using their username and client ID. 
+If no email address is found, it returns false.
+
+.PARAMETER Username
+The username of the Halo user.
+
+.PARAMETER ClientId
+The client ID associated with the Halo user.
+
+.EXAMPLE
+Get-HaloUserEmail -Username "jdoe" -ClientId 12345
+Returns the email address of the Halo user with username 'jdoe' and client ID '12345'.
+#>
+function Get-HaloUserEmail {
+    [CmdletBinding()]
     param (
-        $Username,
-        $ClientId
+        [Parameter(Mandatory)]
+        [string]$Username,
+
+        [Parameter(Mandatory)]
+        [int]$ClientId
     )
-    #Get Email address based on provided username. 
 
-    $address = (Get-HaloUser -Search $username -ClientID $ClientId).emailaddress
+    # Retrieves the email address for a specified Halo user.
+    $address = (Get-HaloUser -Search $Username -ClientID $ClientId).emailaddress
 
-    if ($null -eq $address) {
+    if (-not $address) {
         return $false
     } else { 
         return $address
     }
-    
 }
 
-function SendResponse {
+<#
+.SYNOPSIS
+Sends an email response via Halo for a specific ticket.
+
+.DESCRIPTION
+This function sends an email response through the Halo system. It requires the recipient's email address, 
+a message to be sent, and the ticket ID related to the Halo action.
+
+.PARAMETER EmailAddress
+The email address to which the message will be sent.
+
+.PARAMETER EmailMessage
+The message content to be sent in the email.
+
+.PARAMETER TicketId
+The ticket ID associated with the Halo action.
+
+.EXAMPLE
+Send-HaloEmailResponse -EmailAddress "user@example.com" -EmailMessage "Your issue has been resolved." -TicketId 1001
+Sends an email to 'user@example.com' with the message "Your issue has been resolved." for ticket ID 1001.
+#>
+function Send-HaloEmailResponse {
+    [CmdletBinding()]
     param (
-        $EmailAddress,
-        $EmailMessage,
-        $TicketId
+        [Parameter(Mandatory)]
+        [string]$EmailAddress,
+
+        [Parameter(Mandatory)]
+        [string]$EmailMessage,
+
+        [Parameter(Mandatory)]
+        [int]$TicketId
     )
-    #Send the request through Halo to send an email. 
-    $dateArrival = (get-date((get-date).AddMinutes(-1)))
-    $dateEnd = (get-date) 
-    
+
+    # Sends an email response via Halo.
+    $dateArrival = (Get-Date).AddMinutes(-1)
+    $dateEnd = Get-Date
+
     $ActionUpdate = @{
-        ticket_id               = $TicketId
-        outcome                 = "Email User"
-        outcome_id              = 16
-        emailfrom               = "IT Support"
-        replytoaddress          = "helpdesk@aegis-group.co.uk"
-        emailto                 = $EmailAddress
-        #emailsubject           = ""
-        note                    = $EmailMessage
-        actionarrivaldate       = $dateArrival
-        actioncompletiondate    = $dateEnd
-        action_isresponse       = $false
-        validate_response       = $false
-        sendemail               = $true
+        ticket_id             = $TicketId
+        outcome               = "Email User"
+        outcome_id            = 16
+        emailfrom             = "IT Support"
+        replytoaddress        = "helpdesk@aegis-group.co.uk"
+        emailto               = $EmailAddress
+        note                  = $EmailMessage
+        actionarrivaldate     = $dateArrival
+        actioncompletiondate  = $dateEnd
+        action_isresponse     = $false
+        validate_response     = $false
+        sendemail             = $true
     }
 
-    $Null = New-HaloAction -Action $ActionUpdate
+    New-HaloAction -Action $ActionUpdate
 }
 
-function FindSendResponse {
+<#
+.SYNOPSIS
+Sends an email response via Halo for a specific ticket.
+
+.DESCRIPTION
+This function sends an email response through the Halo system. It requires the recipient's email address, 
+a message to be sent, and the ticket ID related to the Halo action.
+
+.PARAMETER EmailAddress
+The email address to which the message will be sent.
+
+.PARAMETER EmailMessage
+The message content to be sent in the email.
+
+.PARAMETER TicketId
+The ticket ID associated with the Halo action.
+
+.EXAMPLE
+Send-HaloEmailResponse -EmailAddress "user@example.com" -EmailMessage "Your issue has been resolved." -TicketId 1001
+Sends an email to 'user@example.com' with the message "Your issue has been resolved." for ticket ID 1001.
+#>
+function FindAndSendHaloResponse {
+    [CmdletBinding()]
     param (
-        $Username,
-        $clientid,
-        $EmailMessage,
-        $TicketId
+        [Parameter(Mandatory)]
+        [string]$Username,
+
+        [Parameter(Mandatory)]
+        [int]$ClientId,
+
+        [Parameter(Mandatory)]
+        [string]$EmailMessage,
+
+        [Parameter(Mandatory)]
+        [int]$TicketId
     )
 
-    $EmailAddress = GetUserEmail -Username $Username -ClientId $clientid
+    # Retrieves user email and sends a response.
+    $EmailAddress = Get-HaloUserEmail -Username $Username -ClientId $ClientId
 
-    if ($null = $EmailAddress) {
+    if (-not $EmailAddress) {
         return $false
     }
 
-    #Send the request through Halo to send an email. 
-    $dateArrival = (get-date((get-date).AddMinutes(-1)))
-    $dateEnd = (get-date) 
-    
-    $ActionUpdate = @{
-        ticket_id               = $TicketId
-        outcome                 = "Email User"
-        outcome_id              = 16
-        emailfrom               = "IT Support"
-        replytoaddress          = "helpdesk@aegis-group.co.uk"
-        emailto                 = $EmailAddress
-        #emailsubject           = ""
-        note                    = $EmailMessage
-        actionarrivaldate       = $dateArrival
-        actioncompletiondate    = $dateEnd
-        action_isresponse       = $false
-        validate_response       = $false
-        sendemail               = $true
-    }
-
-    $Null = New-HaloAction -Action $ActionUpdate
-
+    Send-HaloEmailResponse -EmailAddress $EmailAddress -EmailMessage $EmailMessage -TicketId $TicketId
     return $true
 }
-
